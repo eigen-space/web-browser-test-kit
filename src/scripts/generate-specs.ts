7a6e696fa5ecb715f580073df081e2509c1a6598
Danilqa <hello.boriskas@gmail.com>
2020-08-08 21:11:05 +0300

    SC-199 app: changed web driver to puppeteer

--- src/scripts/generate-specs.ts
+++ src/scripts/generate-specs.ts
@@ -1,14 +1,14 @@
 import { SpecGenerator } from '@cybernated/web-browser-test-creator';
 import * as path from 'path';
 import { ArgumentParser } from '@eigenspace/argument-parser';
-import { WebDriverIoActionGenerator } from '..';
+import { PuppeteerActionGenerator } from '..';
 
 const parser = new ArgumentParser();
 const args = parser.get(process.argv.slice(2));
 
 const cwd = process.cwd();
 const outputDir = args.get('outputDir') as string || path.join(cwd, './ui-specs/specs');
-const runner = new SpecGenerator(new WebDriverIoActionGenerator(), outputDir);
+const runner = new SpecGenerator(new PuppeteerActionGenerator(), outputDir);
 
 const configsPath = args.get('configDir') as string || path.join(cwd, './ui-specs/configs');
 runner.run(configsPath);
